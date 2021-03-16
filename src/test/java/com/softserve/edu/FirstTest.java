34feb4013bbabee357f8dad17bd6a597a749090c
bebebe <garasum@ukr.net>
2021-03-16 13:40:05 +0200

    test updated

--- src/test/java/com/softserve/edu/FirstTest.java
+++ src/test/java/com/softserve/edu/FirstTest.java
@@ -8,6 +8,11 @@ public class FirstTest {
     @Test
     public void checkOne() {
         System.out.println("FirstTest::checkOne()");
+        System.out.println("MY_PASSWORD = " + System.getenv("MY_PASSWORD"));
+        System.out.println("THYMELEAF_CONNECTION = " + System.getenv("THYMELEAF_CONNECTION"));
+        System.out.println("THYMELEAF_USER = " + System.getenv("THYMELEAF_USER"));
+        System.out.println("THYMELEAF_PSW = " + System.getenv("THYMELEAF_PSW"));
+        System.out.println("JENKINS_VAR = " + System.getenv("JENKINS_VAR"));
         Assertions.assertTrue(true);
     }
 
