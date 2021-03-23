6f289cf8890070960c8ebed218cd3ac7930417e9
bebebe <garasum@ukr.net>
2021-03-23 13:00:42 +0200

    add class Calc

---
+++ UnitTestProjectP312/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectP312
+{
+    public class Calc
+    {
+        public double Add(double arg0, double arg1)
+        {
+            return arg0 + arg1;
+        }
+
+        public string Hello(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
--- UnitTestProjectP312/UnitTest1.cs
+++ UnitTestProjectP312/UnitTest1.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 
 namespace UnitTestProjectP312
 {
-    [TestClass]
+    //[TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        //[TestMethod]
+        [Test]
+        public void TestMethodAdd()
         {
+            Calc calc = new Calc();
+            double expected = 123;
+            double actual;
+            //
+            actual = calc.Add(120, 3);
+            Assert.AreEqual(expected, actual, 0.0001);
+        }
+
+        [Test]
+        public void TestMethodHello()
+        {
+            Calc calc = new Calc();
+            string expected = "Hello Abu";
+            string actual;
+            //
+            actual = calc.Hello("Abu");
+            Assert.AreEqual(expected, actual);
         }
     }
 }
